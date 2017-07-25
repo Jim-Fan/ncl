918ea8025daf800fafba69a370a1caf50e54f6a8
Jim Fan <iamjimfan@gmail.com>
2017-07-25 17:26:38 +0800

    Add handler for SIGINT
    Longjmp then goto cleanup routine upon SIGINT

--- main.c
+++ main.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
+#include <setjmp.h>
+#include <signal.h>
 #include "ncl.h"
 
 extern int yyparse();
 
+static jmp_buf jbuf;
+
+static void sig_handler(int signal)
+{
+    if (signal == SIGINT) longjmp(jbuf, 1);
+}
+
 int main(void)
 {
+    if (signal(SIGINT, sig_handler) == SIG_ERR)
+    {
+        fprintf(stderr, "Error installing signal handler, continue\n");
+    }
+
+    // Crucial to setjmp early, if this is put, for example,
+    // after CLEAN_UP label, longjmp() has no place to jump
+    // to when interrupted during execution loop
+    if (setjmp(jbuf) != 0)
+    {
+        fprintf(stderr, "Interrupted, abort and clean up...\n");
+        goto CLEAN_UP;
+    }
+
     ncl_init();
     printf("(ncl version 0.1)\n");
 
